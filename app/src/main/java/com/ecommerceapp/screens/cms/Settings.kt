eb39dffe60835f15515f35448d2bf56e8cd5d273
Imran <ikhan1057@gmail.com>
2024-09-07 22:33:46 +0530

    updated sdk

--- app/src/main/java/com/ecommerceapp/screens/cms/Settings.kt
+++ app/src/main/java/com/ecommerceapp/screens/cms/Settings.kt
@@ -5,7 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -23,6 +29,7 @@ fun SettingsScreen(navController: NavHostController) {
                 .fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
+            var checked by remember { mutableStateOf(true) }
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -30,6 +37,19 @@ fun SettingsScreen(navController: NavHostController) {
                 AppButton(buttonname = stringResource(id = R.string.changepwd)) {
                     navController.navigate(AppScreenName.CHANGEPASSWORD.name)
                 }
+
+                Switch(
+                    checked = checked,
+                    onCheckedChange = {
+                        checked = it
+                    },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = MaterialTheme.colors.primary,
+                        checkedTrackColor = MaterialTheme.colors.primaryVariant,
+                        uncheckedThumbColor = MaterialTheme.colors.secondary,
+                        uncheckedTrackColor = MaterialTheme.colors.secondaryVariant,
+                    )
+                )
             }
         }
     }
